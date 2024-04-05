11fad94dbfbc17c71210dff6ed59ae3c9047b26d
Kamal Huseynaliyev <kamalrh@code.edu.az>
2024-04-05 17:56:09 +0400

    Initial commit

---
+++ ConsoleToWeb/ConsoleToWeb/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleToWeb.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToWeb
+{
+    internal class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-IGIN0GLR\\SQLEXPRESS;Database=sql;Trusted_connection=true");
+        }
+    }
+}
---
+++ ConsoleToWeb/ConsoleToWeb/Program.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleToWeb;
+using ConsoleToWeb.Entities;
+
+
+AppDbContext appDbContext = new AppDbContext();
+
+
+
+var appBuilder = WebApplication.CreateBuilder();
+
+
+appBuilder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = appBuilder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/", () =>
+{
+    return "Hello, World!";
+});
+
+app.MapGet("/products", () =>
+{
+    return appDbContext.Products.ToList();
+});
+
+app.MapGet("/products/{id}", (int id) =>
+{
+    var data = appDbContext.Products.Find(id);
+
+    if (data == null) return Results.NotFound("Product Not Found!");
+
+    return Results.Ok(data);
+});
+
+app.MapPost("/products", (Product product) =>
+{
+    appDbContext.Add(product);
+    appDbContext.SaveChanges();
+
+    return Results.Ok();
+});
+
+
+app.Run();
