fe0d33727618f233046e290160f17c080b305fad
Kamal Huseynaliyev <kamalrh@code.edu.az>
2024-04-05 20:23:09 +0400

    .

--- ConsoleToWeb/ConsoleToWeb/obj/Debug/net6.0/ConsoleToWeb.AssemblyInfo.cs
+++ ConsoleToWeb/ConsoleToWeb/obj/Debug/net6.0/ConsoleToWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleToWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fad94dbfbc17c71210dff6ed59ae3c9047b26d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleToWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleToWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
